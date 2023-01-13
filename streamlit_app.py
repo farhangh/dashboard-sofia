7c398c055b7c7db9bcaf13738de992990b9fbcee
sofia <siyoucef.sofiaamel@gmail.com>
2023-01-13 17:08:04 +0100

    Merge remote-tracking branch 'origin/master'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
-import streamlit as st
-import requests
-import json
-import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
 from st_functions.streamlit_functions import st_title, st_client_id, st_buttons, plot_g_importance, display_explanation, plot_l_importance
-
-# from lime.lime_tabular import LimeTabularExplainer
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.model_selection import train_test_split
 
 
 st_title()
@@ -18,8 +8,3 @@ plot_g_importance(n_features, b_importance)
 plot_l_importance(b_loc_importance, client_id)
 
 
-
-
-
-
-# if __name__ == '__main__':
